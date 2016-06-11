229d0898b5c2e3708ba9bd5ec502d14c2017ae33
Justin Allred <jjallred.ja@gmail.com>
2016-06-11 11:43:32 -0600

    finished

--- public/ChatController.js
+++ public/ChatController.js
@@ -3,5 +3,19 @@ angular.module('gifsChat')
 .controller('ChatController', function ($scope, apiService) {
 
   // TODO: call giphy api and display gifst to front end
+  $scope.gifsArray = [];
+
+  $scope.sendChat = function() {
+    apiService.getGif($scope.queryText)
+    .then(function(response) {
+      console.warn(response.data.data[0]);
+      $scope.gifsArray.unshift(response.data.data[0]);
+      console.info
+    })
+    .catch(function(err) {
+      return console.error(err);
+
+    });
+  };
 
 });
--- public/apiService.js
+++ public/apiService.js
@@ -2,10 +2,11 @@ angular.module('gifsChat')
 
 .service('apiService', function ($http) {
 
-  this.getGif = function (queryText, callback) {
+  this.getGif = function (queryText) {
     var uri = 'http://api.giphy.com/v1/gifs/search?q=' + queryText + '&api_key=dc6zaTOxFJmzC';
 
     // TODO: do something awesome
+    return $http.get(uri);
   };
 
 });
